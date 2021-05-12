77b2016563bd9c5361f9a38c89cb8bdc80b7c669
PChomyszyn <grarl_ppsp@hotmail.com>
2021-05-12 13:49:51 -0600

    Initial Commit of base game

---
+++ Source/ToonTanks/Actors/ProjectileBase.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ProjectileBase.generated.h"
+
+class UProjectileMovementComponent;
+
+UCLASS()
+class TOONTANKS_API AProjectileBase : public AActor
+{
+	GENERATED_BODY()
+	
+private:
+	//Components
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+		UProjectileMovementComponent* ProjectileMovement;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+		UStaticMeshComponent* ProjectileMesh;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Effects", meta = (AllowPrivateAccess = "true"))
+		UParticleSystemComponent* ParticleTrail;
+
+	//Variables
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+		TSubclassOf<UDamageType> DamageType;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Move", meta = (AllowPrivateAccess = "true"))
+		float MovementSpeed = 1300.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage", meta = (AllowPrivateAccess = "true"))
+		float Damage = 50.0f;
+	UPROPERTY(EditAnywhere, Category = "Effects")
+		UParticleSystem* HitParticle;
+	UPROPERTY(EditAnywhere, Category = "Effects")
+		USoundBase* HitSound;
+	UPROPERTY(EditAnywhere, Category = "Effects")
+		USoundBase* LaunchSound;
+	UPROPERTY(EditAnywhere, Category = "Effects")
+		TSubclassOf<UMatineeCameraShake> HitShake;
+
+	//Functions
+	UFUNCTION()
+		void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+public:	
+	// Sets default values for this actor's properties
+	AProjectileBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+};
